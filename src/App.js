8c072d420660f4abab769d2f088c452998047eff
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-06-26 09:51:32 +0100

    added projects and got them visible

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,59 @@
-import projectImage1 from "../assets/images/project-img1.png";
-import projectImage2 from "../assets/images/project-img2.png";
-import projectImage3 from "../assets/images/projects-img3.png";
+import React from 'react';
+import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
+import { ProjectCard } from './ProjectCard';
+import projectImage1 from '../assets/images/project-img1.png';
+import projectImage2 from '../assets/images/project-img2.png';
+import projectImage3 from '../assets/images/project-img3.png';
 import 'animate.css';
-import TrackVisibility from "react-on-screen";
-import { projectCard } from "./ProjectCard";
+import TrackVisibility from 'react-on-screen';
 
+export const Projects = () => {
+  const projects = [
+    {
+      title: 'Weather Dashboard',
+      description: 'This was an app created for our first group project.',
+      imageUrl: projectImage1,
+    },
+    {
+      title: 'The Sequel',
+      description: 'This was an app created for our second group project.',
+      imageUrl: projectImage2,
+    },
+    {
+      title: 'Developer Blog',
+      description: 'This was an app created for one of my assignments.',
+      imageUrl: projectImage3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div className={isVisible ? 'animate__animated animate__fadeIn' : ''}>
+                  <h2>Projects</h2>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type
+                    specimen book.
+                  </p>
+                  <Row>
+                    {projects.map((project, index) => (
+                      <Col key={index} md={4}>
+                        <ProjectCard {...project} />
+                      </Col>
+                    ))}
+                  </Row>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
