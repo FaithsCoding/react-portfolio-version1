108673d530613362f6b6bd39964792d1b34e1b91
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-06-26 09:29:00 +0100

    added imports for projects and projectcard file

---
+++ src/components/Projects.js
@@ -0,0 +1,6 @@
+import projectImage1 from "../assets/images/project-img1.png";
+import projectImage2 from "../assets/images/project-img2.png";
+import projectImage3 from "../assets/images/projects-img3.png";
+import 'animate.css';
+import TrackVisibility from "react-on-screen";
+import { projectCard } from "./ProjectCard";
